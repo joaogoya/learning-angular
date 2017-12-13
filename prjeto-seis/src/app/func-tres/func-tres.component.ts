8eebddb99b1aa3b05bcee439e7b9736324c7e516
Joao Goya <joaogoya@gmail.com>
2017-12-13 16:33:29 -0200

    comit depois da aula 11 de event biding

--- prjeto-seis/src/app/func-tres/func-tres.component.ts
+++ prjeto-seis/src/app/func-tres/func-tres.component.ts
@@ -9,6 +9,18 @@ export class FuncTresComponent implements OnInit {
 
   constructor() { }
 
+  btClicado(){
+    alert('clicou!');
+  }
+
+  valorAtual:string;
+  inputKeyUp(evento: KeyboardEvent){
+    //console.log(evento);
+    console.log((<HTMLInputElement>evento.target).value);
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+
+  }
+
   ngOnInit() {
   }
 
