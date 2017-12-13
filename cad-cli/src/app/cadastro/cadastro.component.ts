1c8b269bd5e3d0fb544fa417e57d269329d20ad8
Joao Goya <joaogoya@gmail.com>
2017-12-13 17:51:28 -0200

    inicio do form

---
+++ cad-cli/src/app/cadastro/cadastro.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro',
+  templateUrl: './cadastro.component.html',
+  styleUrls: ['./cadastro.component.css']
+})
+export class CadastroComponent implements OnInit {
+
+  nomes: string[];
+  telefones: number[];
+  contador: number = 0;
+
+  constructor() { }
+
+  v ;
+    capturaElemento(){
+      this.v = document.getElementById("Text1").value;
+      console.log(this.v);
+      this.contador++;
+      
+    }
+
+  ngOnInit() {
+  }
+
+}
