076f6dcd488a1ea50e0c60ff9776bea5f48ed9c6
Joao Goya <joaogoya@gmail.com>
2017-12-13 09:21:41 -0200

    comoit qd terminei aula 6 - templates

--- projeto-tres/src/app/componente-solto-01/componente-solto-01.component.ts
+++ projeto-tres/src/app/componente-solto-01/componente-solto-01.component.ts
@@ -5,4 +5,20 @@ import { Component } from '@angular/core';
   templateUrl: './componente-solto-01.component.html',
   styleUrls: ['./componente-solto-01.component.css']
 })
-export class ComponenteSolto01Component {}
+export class ComponenteSolto01Component {
+
+  nome : string ;
+
+  cursos: string[] =  ['lorem', 'imspum', 'dolor'];
+
+  constructor() {
+      this.nome = "Apenas uma string";
+
+      /*assim é um for normal, mas da pra fazer no template, com uma diretiva
+      for( let i = 0; i <= this.cursos.length; i++){
+          let curso = this.cursos[i];
+      }
+      */
+    }
+
+}
