3fc453eae68ca80107baa213c158958ced58e562
Joao Goya <joaogoya@gmail.com>
2018-01-03 15:37:22 -0200

    rotas, até a aula 56

--- rotas/src/app/cursos/cursos.component.ts
+++ rotas/src/app/cursos/cursos.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CursosService } from './cursos.service';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cursos',
@@ -9,12 +12,30 @@ import { CursosService } from './cursos.service';
 export class CursosComponent implements OnInit {
 
   cursos:any[]; 
+  pagina:number;
+  inscricao: Subscription;
 
-  constructor(private cursoSService: CursosService) {
+  constructor(
+    private cursoSService: CursosService,
+    private activateRoute: ActivatedRoute,
+    private router: Router
+    ) {
   }
 
   ngOnInit() {
     this.cursos = this.cursoSService.getCursos();
+    this.inscricao = this.activateRoute.queryParams.subscribe(
+      (queryParams) => {
+        this.pagina = queryParams['pagina'];
+      }
+    );
+  }
+  aumentaPagina(){
+    this.pagina ++;
+    this.router.navigate(['/cursos'], {queryParams: { 'pagina': this.pagina }});
+  }
+  ngOnDestroy(){
+    this.inscricao.unsubscribe();
   }
 
 }
